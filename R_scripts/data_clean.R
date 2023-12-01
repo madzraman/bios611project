38c7fa01cf0f09580e057410e827bcdc5d5f7cd7
madhurir <madhurir@unc.edu>
2023-12-01 21:44:46 +0000

    commented out anything being printed

--- R_scripts/data_clean.R
+++ R_scripts/data_clean.R
@@ -11,8 +11,8 @@ df_original <- read_csv("source_data/flight_delays_2022_onethird.csv")
 
 df_original <- df_original[,-ncol(df_original)]
 
-sum(df_original$DepTime >= df_original$ArrTime)
-sum(df_original$DepTime >= df_original$ArrTime)/nrow(df_original)
+# sum(df_original$DepTime >= df_original$ArrTime)
+# sum(df_original$DepTime >= df_original$ArrTime)/nrow(df_original)
 
 
 
@@ -20,9 +20,9 @@ sum(df_original$DepTime >= df_original$ArrTime)/nrow(df_original)
 
 
 
-head(df_original[df_original$DepTime >= df_original$ArrTime, c("DepTime", "ArrTime")])
-mean(df_original[df_original$DepTime >= df_original$ArrTime,]$DepTime)
-mean(df_original[df_original$DepTime >= df_original$ArrTime,]$ArrTime)
+# head(df_original[df_original$DepTime >= df_original$ArrTime, c("DepTime", "ArrTime")])
+# mean(df_original[df_original$DepTime >= df_original$ArrTime,]$DepTime)
+# mean(df_original[df_original$DepTime >= df_original$ArrTime,]$ArrTime)
 
 
 # As a reminder, the departure and arrival times are formatted as "HHMM", i.e. hours and minutes. Note that since these are numeric data types, it does not make sense to add and subtract integers from them. By examining the sample of data above, at first glance it seems odd that there are arrival times with less than 4 digits. However, we then realize that the missing leading digits are simply ones, as the "HHMM" format is in military (24-hour) time. Thus, `610` really means `0610` like 6:10am, and `7` really means `0007` like 12:07am. Let us quickly verify that for all the "time" variables in "HHMM" format, i.e. `CRSDepTime`, `DepTime`, `CRSArrTime`, `ArrTime`, `WheelsOff`, and `WheelsOn`, that the "HH" part is always between 0 and 24 and that the "MM" part is always between 0 and 59. We do this below. We will also add these hours and minutes individually as columns in our dataframe for any future use.
@@ -44,13 +44,13 @@ validate_minutes <- function(cv){
     return (sum(ifelse( 0 <= cv & cv <= 59, 0, 1))) # 1 for invalids, 0 for valids
 }
 
-sum(validate_hours(df_new$DepTime_Hours), validate_hours(df_new$CRSDepTime_Hours), 
-    validate_hours(df_new$ArrTime_Hours), validate_hours(df_new$CRSArrTime_Hours), 
-    validate_hours(df_new$WheelsOff_Hours), validate_hours(df_new$WheelsOn_Hours))
-
-sum(validate_minutes(df_new$DepTime_Minutes), validate_minutes(df_new$CRSDepTime_Minutes), 
-    validate_minutes(df_new$ArrTime_Minutes), validate_minutes(df_new$CRSArrTime_Minutes), 
-    validate_minutes(df_new$WheelsOff_Minutes), validate_minutes(df_new$WheelsOn_Minutes))
+# sum(validate_hours(df_new$DepTime_Hours), validate_hours(df_new$CRSDepTime_Hours), 
+#     validate_hours(df_new$ArrTime_Hours), validate_hours(df_new$CRSArrTime_Hours), 
+#     validate_hours(df_new$WheelsOff_Hours), validate_hours(df_new$WheelsOn_Hours))
+# 
+# sum(validate_minutes(df_new$DepTime_Minutes), validate_minutes(df_new$CRSDepTime_Minutes), 
+#     validate_minutes(df_new$ArrTime_Minutes), validate_minutes(df_new$CRSArrTime_Minutes), 
+#     validate_minutes(df_new$WheelsOff_Minutes), validate_minutes(df_new$WheelsOn_Minutes))
 
 
 # The above sums being zero mean that all of our hours and minutes for all time variables are valid.
