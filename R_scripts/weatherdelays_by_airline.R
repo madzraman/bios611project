1784e10ba127772dae8b1bb8895ae3de3393dc02
madhurir <madhurir@unc.edu>
2023-12-13 22:03:16 +0000

    updated title

--- R_scripts/weatherdelays_by_airline.R
+++ R_scripts/weatherdelays_by_airline.R
@@ -6,14 +6,30 @@ library(ggplot2)
 df <- read_csv("derived_data/df_original_cleaned.csv")
 
 # bar plot of departure delays by each airline code
-bp_weatherdelay_airline <- ggplot(df) + 
+total_bp_weatherdelay_airline <- ggplot(df) + 
   geom_bar(aes(x=fct_infreq(Marketing_Airline_Network), fill=sum(WeatherDelay))) +
-  labs(title="Airlines with the most Weather Delay in 2022", 
+  labs(title="Airlines with the Most Weather Delay in Total in 2022", 
        x='Airline', 
-       y='total minutes of weather delay ') + 
+       y='Total Minutes of Weather Delay') + 
   theme(legend.position="none")
 
 
-ggsave("figures/weatherdelays_by_airline.png", plot=bp_weatherdelay_airline)
+ggsave("figures/total_weatherdelays_by_airline.png", plot=total_bp_weatherdelay_airline)
 
 # The most flight delays due to weather (in terms of total minutes delayed) occur from American Airlines, Delta, and Southwest, which make sense because they are the most popular airlines and probably have the most flights to begin with. We should consider scaling this plot by the total number of flights, as this would be more meaningful for comparison. (Will do later)
+
+
+# Now we scaled the plot of weather delays by airlines by the number of flights
+
+# SCALED bar plot of departure delays by each airline code
+# AVERAGE number of minutes of weather delay for each airline
+# 
+# df$Marketing_Airline_Network <- as.factor(df$Marketing_Airline_Network)
+# average_bp_weatherdelay_airline <- ggplot(df) + 
+#   geom_bar(aes(x=fct_infreq(Marketing_Airline_Network), fill=sum(WeatherDelay)/count(WeatherDelay))) +
+#   labs(title="Airlines with the Most Weather Delay On Average in 2022", 
+#        x='Airline', 
+#        y='Average Minutes of Weather Delay') + 
+#   theme(legend.position="none")
+# 
+# ggsave("figures/average_weatherdelays_by_airline.png", plot=average_bp_weatherdelay_airline)
